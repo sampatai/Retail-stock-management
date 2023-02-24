02e25b0a72085033d36a10e2aaceca4b709bd12d
sampat_adhikari <sampat.adhikari@omnicommediagroup.com>
2023-02-24 15:39:56 +0545

    added dependency injection

---
+++ LiquorInStock.Application/Common/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Retail.Stock.Domain.Aggregates.Category;
+using Retail.Stock.Shared.SeedWork;
+
+namespace Retail.Stock.Application.Common
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
--- LiquorInStock.UI/Form1.cs
+++ LiquorInStock.UI/Form1.cs
@@ -1,10 +1,36 @@
+using Microsoft.Extensions.Logging;
+
 namespace LiquorInStock.UI
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        //public Form1()
+        //{
+            
+
+        //}
+        private readonly ILogger _logger;
+        public Form1(ILogger<Form1> logger)
         {
             InitializeComponent();
+            _logger = logger;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _logger.LogInformation("Form1 {BusinessLayerEvent} at {dateTime}", "Started", DateTime.UtcNow);
+                // Perform Business Logic here 
+
+                MessageBox.Show("Hello .NET Core 3.1.This is First Forms app in .NET Core");
+                _logger.LogInformation("Form1 {BusinessLayerEvent} at {dateTime}", "Ended", DateTime.UtcNow);
+            }
+            catch (Exception ex)
+            {
+                //Log technical exception 
+                _logger.LogError(ex.Message);
+            }
         }
     }
 }
